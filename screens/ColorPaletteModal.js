75e69eac666254b48c4269dca553a100eb5caa02
OgheneMichael <zonalmeet@gmail.com>
2020-07-05 00:54:29 +0100

    Created text input... used alert module for error message

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    if (!name) {
+      Alert.alert('Please, enter a palette name');
+    }
+  }, [name]);
+
   return (
-    <View>
-      <Text>Hello, I am a modal!</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setName}
+        value={name}
+        placeholder="Palette name"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 50,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  name: {
+    marginBottom: 10,
+  },
+});
 
 export default ColorPaletteModal;
